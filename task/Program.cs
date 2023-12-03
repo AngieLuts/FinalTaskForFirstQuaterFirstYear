54ca984d62eb7e5116ed6f01a0f61a5b35abf1d2
AngieLuts <angiegk@yandex.ru>
2023-12-03 14:48:29 +0300

    wrote methods for solving the task

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] TypeIn(string text) //Метод для ввода строки от пользователя
+{
+    System.Console.WriteLine(text);
+    string[] array = Console.ReadLine().Split(" ");
+    return array;
+}
+int Randomnumber(int anyvalue) // Создание случайного числа в заданных границах
+{
+    Random rnd = new Random();
+    int randomnumber = rnd.Next(0, anyvalue);
+    return randomnumber;
+
+}
+void PrintArray(string[] array) // Печать массива
+{
+    foreach (var item in array)
+    {
+        Console.Write(item + " ");
+    }
+
+}
